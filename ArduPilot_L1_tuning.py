e09a3ebbb42ac9311403f86ff5ff8581f37bfcc7
TunaLobster <charliepjohnson@gmail.com>
2022-04-29 10:36:47 -0500

    Fix sliders and make yaxis limits update

--- ArduPilot_L1_tuning.py
+++ ArduPilot_L1_tuning.py
@@ -19,7 +19,7 @@ L1_dist = max(0.3183099 * L1_damp * L1_period * groundspeed, dist_min)
 def f(turn_angle, groundspeed, pressure, tempK, L1_damp, L1_period, wp_radius):
     # equivalent airspeed to true airspeed ratio
     eas2tas = (1.225 / (pressure / (287.26 * tempK))) ** 0.5
-    L1_dist = get_L1_dist(groundspeed, L1_damp, L1_period,)
+    L1_dist = get_L1_dist(groundspeed, L1_damp, L1_period)
     return np.array([turn_distance(i, wp_radius, eas2tas, L1_dist) for i in turn_angle])
 
 
@@ -31,8 +31,8 @@ def f_L1_dist(x, groundspeed, L1_damp, L1_period):
     return np.ones_like(x) * get_L1_dist(groundspeed, L1_damp, L1_period)
 
 
-def f_wp_radius(x):
-    return np.ones_like(x) * WP_radius
+def f_wp_radius(x, wp_radius):
+    return np.ones_like(x) * wp_radius
 
 
 def turn_distance(turn_angle, wp_radius, eas2tas, L1_dist):
@@ -47,7 +47,7 @@ turn_angle_plot = np.linspace(0, 100, 300)
 fig, ax = plt.subplots()
 line, = plt.plot(turn_angle_plot, f(turn_angle_plot, groundspeed, pressure, tempK, L1_damp, L1_period, WP_radius), label="Start turn distance")
 l1_dist_line, = plt.plot(turn_angle_plot, f_L1_dist(turn_angle_plot, groundspeed, L1_damp, L1_period), label="L1_dist {:.2f} m".format(L1_dist))
-wp_radius_line, = plt.plot(turn_angle_plot, f_wp_radius(turn_angle_plot), label="WP_RADIUS {:.2f} m".format(WP_radius))
+wp_radius_line, = plt.plot(turn_angle_plot, f_wp_radius(turn_angle_plot, WP_radius), label="WP_RADIUS {:.2f} m".format(WP_radius))
 
 # make room for sliders
 plt.subplots_adjust(bottom=0.32)
@@ -57,7 +57,7 @@ axgroundspeed = plt.axes([0.25, 0.2, 0.65, 0.02])
 groundspeed_slider = Slider(
     ax=axgroundspeed,
     label="Groundspeed [m/s]",
-    valmin=20,
+    valmin=15,
     valmax=100,
     valinit=groundspeed,
 )
@@ -65,7 +65,7 @@ groundspeed_slider = Slider(
 
 # make a horizontal slider to control the L1_damping
 axl1damping = plt.axes([0.25, 0.15, 0.65, 0.02])
-L1damping_slider = Slider(
+l1damping_slider = Slider(
     ax=axl1damping,
     label="NAVL1_DAMPING",
     valmin=0.5,
@@ -90,20 +90,19 @@ axwpradius = plt.axes([0.25, 0.05, 0.65, 0.02])
 wpradius_slider = Slider(
     ax=axwpradius,
     label="WP_RADIUS [m]",
-    valmin=100,
-    valmax=550,
+    valmin=20,
+    valmax=600,
     valinit=WP_radius,
 )
 
 
 def update(val):
-    line.set_ydata(f(turn_angle_plot, groundspeed_slider.val, pressure, tempK, L1damping_slider.val, l1period_slider.val, wpradius_slider.val))
-    # ax.set_title("Groundspeed = {:.2f} m/s,\nNAVL1_DAMPING = {}, NAVL1_PERIOD = {}".format(groundspeed_slider.val, L1damping_slider.val, l1period_slider.val, wpradius_slider.val))
-    l1_dist_line.set_ydata(f_L1_dist(turn_angle_plot, groundspeed_slider.val, L1damping_slider.val, l1period_slider.val))
-    l1_dist_line.set(label = "L1_dist {:.2f} m".format(get_L1_dist(groundspeed_slider.val, L1damping_slider.val, l1period_slider.val)))
-    wp_radius_line.set_ydata(f_wp_radius(turn_angle_plot))
-    ax.set_ylim(auto=True)
-    ax.set_autoscale_on(True)
+    line.set_ydata(f(turn_angle_plot, groundspeed_slider.val, pressure, tempK, l1damping_slider.val, l1period_slider.val, wpradius_slider.val))
+    l1_dist_line.set_ydata(f_L1_dist(turn_angle_plot, groundspeed_slider.val, l1damping_slider.val, l1period_slider.val))
+    l1_dist_line.set(label = "L1_dist {:.2f} m".format(get_L1_dist(groundspeed_slider.val, l1damping_slider.val, l1period_slider.val)))
+    wp_radius_line.set_ydata(f_wp_radius(turn_angle_plot, wpradius_slider.val))
+    wp_radius_line.set(label = "WP_RADIUS {:.2f} m".format(wpradius_slider.val))
+    ax.relim()
     ax.autoscale_view()
     ax.legend(loc="lower right")
     fig.canvas.draw_idle()
@@ -111,17 +110,24 @@ def update(val):
 
 # register the update function with each slider
 groundspeed_slider.on_changed(update)
+l1damping_slider.on_changed(update)
+l1period_slider.on_changed(update)
+wpradius_slider.on_changed(update)
+
 
 # Create a `matplotlib.widgets.Button` to reset the sliders to initial values.
 resetax = plt.axes([0.8, 0.005, 0.1, 0.04])
-button = Button(resetax, 'Reset', hovercolor='0.975')
+resetbutton = Button(resetax, 'Reset', hovercolor='0.975')
 
 
 def reset(event):
     groundspeed_slider.reset()
+    l1damping_slider.reset()
+    l1period_slider.reset()
+    wpradius_slider.reset()
 
 
-button.on_clicked(reset)
+resetbutton.on_clicked(reset)
 
 # ax.set_title("Groundspeed = {:.2f} m/s,\nNAVL1_DAMPING = {}, NAVL1_PERIOD = {}".format(groundspeed, L1_damp, L1_period, WP_radius))
 ax.set_title("Tuning ArduPilot L1")
