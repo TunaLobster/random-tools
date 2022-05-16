1882f9ccc473c3b51b8b771995b702a4520fff13
TunaLobster <charliepjohnson@gmail.com>
2022-05-16 09:55:27 -0500

    Change to searching ArduPilot org

--- AP_PRList.py
+++ AP_PRList.py
@@ -1,6 +1,6 @@
 import json, requests
 
-r = requests.get("https://api.github.com/search/issues?q=is:pr%20label:DevCallTopic%20repo:ardupilot/ardupilot")
+r = requests.get("https://api.github.com/search/issues?q=is:pr%20label:DevCallTopic%20org:ArduPilot")
 j = json.loads(r.content)
 
 for pr in j["items"]:
