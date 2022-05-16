5c5b376969de70ae8f2511ebd1612d4f7b2876f6
TunaLobster <charliepjohnson@gmail.com>
2022-05-16 13:22:16 -0500

    Add issue list and a little bit of formatting

--- AP_PRList.py
+++ AP_PRList.py
@@ -1,7 +1,15 @@
+from curses import pair_content
 import json, requests
 
-r = requests.get("https://api.github.com/search/issues?q=is:pr%20label:DevCallTopic%20org:ArduPilot")
-j = json.loads(r.content)
+pr_search = json.loads(requests.get("https://api.github.com/search/issues?q=is:pr%20label:DevCallTopic%20org:ArduPilot").content)
+issue_search = json.loads(requests.get("https://api.github.com/search/issues?q=is:issue%20label:DevCallTopic%20org:ArduPilot").content)
 
-for pr in j["items"]:
+print("**Pull Requests:**\n")
+for pr in pr_search["items"]:
     print(pr["html_url"])
+
+print("\n")
+
+print("**Issues:**\n")
+for issue in issue_search["items"]:
+    print(issue["html_url"])
