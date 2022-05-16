a7a531f9b18f0c73bcb3da1dd7946c179f77e92c
TunaLobster <charliepjohnson@gmail.com>
2022-05-16 13:29:33 -0500

    Sort PRs oldest to newest

--- AP_PRList.py
+++ AP_PRList.py
@@ -1,11 +1,15 @@
-from curses import pair_content
-import json, requests
+import json
+import requests
+from operator import itemgetter
 
 pr_search = json.loads(requests.get("https://api.github.com/search/issues?q=is:pr%20label:DevCallTopic%20org:ArduPilot").content)
 issue_search = json.loads(requests.get("https://api.github.com/search/issues?q=is:issue%20label:DevCallTopic%20org:ArduPilot").content)
 
+# sort the PRs oldest to newest based on PR number
+pr_sorted = sorted(pr_search["items"], key=itemgetter("number"))
+
 print("**Pull Requests:**\n")
-for pr in pr_search["items"]:
+for pr in pr_sorted:
     print(pr["html_url"])
 
 print("\n")
